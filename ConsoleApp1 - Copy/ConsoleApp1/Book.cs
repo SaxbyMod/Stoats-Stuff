0694271a5ff4e3e3a67bb2a00e76681956d641d8
Geck Man <102002463+SaxbyMod@users.noreply.github.com>
2022-10-02 14:02:51 -0400

    Add files via upload

--- ConsoleApp1 - Copy/ConsoleApp1/Book.cs
+++ ConsoleApp1 - Copy/ConsoleApp1/Book.cs
@@ -11,5 +11,14 @@ namespace ConsoleApp1
         public string title;
         public string author;
         public int pages;
+
+        public Book1(string aTitle, string aAuthor, int aPages)
+        {
+            Console.WriteLine("Creating Book with title of " + aTitle + "\n");
+
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
     }
 }
--- ConsoleApp1 - Copy/ConsoleApp1/Program.cs
+++ ConsoleApp1 - Copy/ConsoleApp1/Program.cs
@@ -11,18 +11,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Book1 book1 = new Book1();
-            book1.title = "Doctor who and the towers of Askaban";
-            book1.author = "Myself";
-            book1.pages = 800;
+            Book1 book1 = new Book1("Doctor who and the towers of Askaban", "Myself", 800);
 
-            Book1 book2 = new Book1();
-            book2.title = "Doctor who and the leaning towers of Askaban";
-            book2.author = "Myself";
-            book2.pages = 1800;
+            Console.WriteLine("The title of this book is " + book1.title + "and The author of this book is " + book1.author + ". \n The amount of pages in this Book is " + book1.pages + ".\n");
 
-            Console.WriteLine("The title of this book is " + book1.title + "and The author of this book is " + book1.author + ". \n The amount of pages in this Book is " + book1.pages + ".");
-            Console.WriteLine("The title of this book is " + book2.title + "and The author of this book is " + book2.author + ". \n The amount of pages in this Book is " + book2.pages + ".");
+            Book1 book2 = new Book1("Doctor who and the leaning towers of Askaban", "Myself", 1800);
+            
+            Console.WriteLine("The title of this book is " + book2.title + "and The author of this book is " + book2.author + ". \n The amount of pages in this Book is " + book2.pages + ".\n");
 
             Console.ReadLine();
         }
