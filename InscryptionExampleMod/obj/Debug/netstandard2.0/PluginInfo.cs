9c850a4e445b4d10927e9a9112eb9c07d92224bb
Geck Man <102002463+SaxbyMod@users.noreply.github.com>
2023-05-28 23:00:44 -0400

    Delete InscryptionExampleMod directory

--- InscryptionExampleMod/obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ExampleMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ExampleMod";
-        public const string PLUGIN_NAME = "ExampleMod";
-        public const string PLUGIN_VERSION = "2.4.0";
-    }
-}
--- InscryptionExampleMod/obj/Release/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ExampleMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "TribeLibary";
-        public const string PLUGIN_NAME = "TribeLibary";
-        public const string PLUGIN_VERSION = "1.10.0";
-    }
-}
