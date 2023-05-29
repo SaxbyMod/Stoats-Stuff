71887b3d713e567c1633b5a1c472820073a10a2a
Geck Man <102002463+SaxbyMod@users.noreply.github.com>
2023-05-28 23:01:22 -0400

    Add files via upload

---
+++ InscryptionExampleMod/obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ExampleMod
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "ATribeLibary";
+        public const string PLUGIN_NAME = "ATribeLibary";
+        public const string PLUGIN_VERSION = "1.13.0";
+    }
+}
