11e1ad91e5459642ae67587c55976d3e2841ce0a
Geck Man <102002463+SaxbyMod@users.noreply.github.com>
2023-05-28 22:59:01 -0400

    Delete obj directory

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ExampleMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ATribeLibary";
-        public const string PLUGIN_NAME = "ATribeLibary";
-        public const string PLUGIN_VERSION = "1.13.0";
-    }
-}
